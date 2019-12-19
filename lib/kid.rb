40587d9663e4b53f08cc6003361908bb52ad0355
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-19 03:25:24 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 class Kid 
   include Dance 
   
@@ -11,4 +12,5 @@ class Kid
     extend MetaDancing
     extend FancyDance :: ClassMethods 
     include FancyDance :: InstanceMethods 
+end 
 end 
