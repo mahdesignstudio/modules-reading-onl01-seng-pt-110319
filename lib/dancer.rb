9b52ffc36876b396a0baf862fe154a2ef5d1e8bd
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-19 03:27:10 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,17 +2,12 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer 
-  
-  include Dance 
-  
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-  end 
-    extend MetaDancing
-  end 
-    extend FancyDance :: ClassMethods 
-    include FancyDance :: InstanceMethods 
+    include FancyDance::InstanceMethods
+    extend FancyDance::ClassMethods
+
+    attr_accessor :name 
+
+    def initialize (name)
+            @name = name
+    end
 end
-end 
